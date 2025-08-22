793cd9c76c3f1a7a3c21b2b8bc025c63a9d9dab0
j-wut <jwut@jwut.dev>
2025-08-22 05:49:31 +0000

    60 fps

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    deltaTime = 0
+    i = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0))
+        pygame.Surface.fill(screen,(i%255,i%255,i%255))
         pygame.display.flip()
+        i += 1
+        deltaTime = clock.tick(60)
 
 
 if __name__ == "__main__":
