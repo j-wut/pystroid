8eb623e7f473c68f9ab6811d0b86429479e2800b
j-wut <jwut@jwut.dev>
2025-08-24 20:51:34 +0000

    fixing momentum calcs

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,6 @@
 
 import pygame
+import random
 
 from circleshape import CircleShape
 from constants import ASTEROID_MIN_RADIUS, SCREEN_WIDTH, SCREEN_HEIGHT
@@ -19,9 +20,11 @@ class Asteroid(CircleShape):
 
     def split(self, shot):
         self.kill()
+        splitRatio = random.uniform(0.4, 0.6)
         if self.radius >= ASTEROID_MIN_RADIUS * 2:
-            a1 = Asteroid(self.position.x, self.position.y, self.radius - ASTEROID_MIN_RADIUS)
-            a2 = Asteroid(self.position.x,self.position.y, self.radius - ASTEROID_MIN_RADIUS)
-            a1.velocity += shot.velocity.rotate(45) * shot.radius / self.radius
-            a2.velocity -= shot.velocity.rotate(45) * shot.radius / self.radius
-
+            a1 = Asteroid(self.position.x, self.position.y, self.radius * splitRatio)
+            a2 = Asteroid(self.position.x,self.position.y, self.radius * (1-splitRatio))
+            a1.velocity = self.velocity.copy()
+            a2.velocity = self.velocity.copy()
+            a1.velocity += shot.velocity.rotate(45) * shot.radius / (2 * self.radius * splitRatio)
+            a2.velocity -= shot.velocity.rotate(45) * shot.radius / (2 * self.radius * (1 - splitRatio))
